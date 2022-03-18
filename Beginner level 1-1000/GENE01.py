26443815420316bd1634827f5882f746619f86f6
Ragesh <84183052+rageshx@users.noreply.github.com>
2022-03-18 15:39:23 +0530

    Genes

---
+++ Beginner level 1-1000/GENE01.py
@@ -0,0 +1,12 @@
+x,y=map(str,input().split())
+
+
+if(x==y):
+    print(x)
+else:
+    if(x=="R" or y=="R"):
+        print("R")
+        exit()
+    if(x=="B" or y=="B"):
+        print("B")
+        
